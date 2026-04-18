a6bf0ae1f8f3965436c79838add0d9e8b5947ac6
Oleh Solomko <solo.oleg1987@gmail.com>
2026-04-18 22:00:54 +0300

    fix: remove postgres config

--- lesson-8-9/web/goitproject/settings.py
+++ lesson-8-9/web/goitproject/settings.py
@@ -115,16 +115,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
-
-import os
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
-        "PORT": os.environ.get("POSTGRES_PORT", "5432"),
-        "NAME": os.environ.get("POSTGRES_DB", "postgres"),
-        "USER": os.environ.get("POSTGRES_USER", "postgres"),
-        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", ""),
-    }
-}
